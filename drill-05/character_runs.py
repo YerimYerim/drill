360d14a6f19d05a129654fa4cb40ffd90b0c6f8f
YerimYerim <vigvang_bip@naver.com>
2019-10-02 18:52:42 +0900

    Initial commit

---
+++ drill-05/character_runs.py
@@ -0,0 +1,34 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('run_animation.png')
+
+x = 0
+
+# 여기를 채우세요.
+frame = 0
+while True:
+    if x < 800:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame+1) % 8
+        x += 10
+        delay(0.04)
+        get_events()
+    elif x >= 800:
+        if x > 1600:
+            x = 0
+        clear_canvas()
+        grass.draw(400, 30)
+        ##character.clip_draw(frame * 100, 0, 100, 100, 800 - x + 800, 90)
+        character.clip_composite_draw(frame * 100, 0, 100, 100, 0, 'h', 800 - x + 800, 90, 100, 100)
+        update_canvas()
+        frame = (frame+1) % 8
+        x += 10
+        delay(0.04)
+        get_events()
+
+close_canvas()
+
