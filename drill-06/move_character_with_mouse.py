1149dc5436f0beaaa3ff261efdea972251db2a96
YerimYerim <vigvang_bip@naver.com>
2019-10-07 19:50:01 +0900

    drill 05 완성
    2018182027 이예림
    https://www.youtube.com/watch?v=5Ucz2IQKH34&feature=youtu.be
    커밋횟수 : 3번 (이거 커밋까지 포함하면 4번)

--- drill-06/move_character_with_mouse.py
+++ drill-06/move_character_with_mouse.py
@@ -1,15 +1,19 @@
 from pico2d import *
+import math
+dir = 0
 i = 0
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 ismove = False
 goal_x , goal_y = 0, 0
+start_x, start_y = 0, 0
 def handle_events():
     # fill here
     global ismove
     global running
-    global x, y
+    global x, y, dir
     global px, py
     global goal_x, goal_y, i
+    global start_x, start_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -18,9 +22,13 @@ def handle_events():
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
             goal_x, goal_y = event.x, KPU_HEIGHT - 1 - event.y
+            start_x, start_y = px, py
             i = 0
             ismove = True
-
+            if px > goal_x:
+                dir = 0
+            elif px < goal_x:
+                dir = 1
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -38,11 +46,14 @@ x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
 px, py = KPU_WIDTH // 2, KPU_HEIGHT // 2
+def distance(p1, p2):
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[0] - p2[0]) ** 2)
 
 def move(p1,p2):
     global px, py
     global i
-    t = i / 100
+    i += distance(p1, p2) / 15
+    t = i / distance(p1, p2)
     px = (1-t)*p1[0]+t*p2[0]
     py = (1-t)*p1[1]+t*p2[1]
 
@@ -50,13 +61,14 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     if ismove:
-        i += 2
-        move((px, py), (goal_x, goal_y))
-    if px > goal_x:
+        move((start_x, start_y), (goal_x, goal_y))
+        if px == goal_x or py == goal_y:
+            ismove = False
+    if dir is 0:
         character.clip_draw(frame * 100, 100 * 0, 100, 100, px - 50, py + 50)
-    else:
+    elif dir is 1:
         character.clip_draw(frame * 100, 100 * 1, 100, 100, px - 50, py + 50)
-
+    print(px, py)
     cusor.clip_draw(0, 0, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
